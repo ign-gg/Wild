b04daec14a528ae517eee36a48effe7ad5a063c4
PetteriM1 <petteri.malkavaara1@gmail.com>
2019-06-14 11:54:06 +0300

    Initial commit

---
+++ src/main/java/tk/suomicraftpe/commands/WildCommand.java
@@ -0,0 +1,52 @@
+package tk.suomicraftpe.commands;
+
+import java.util.Random;
+import cn.nukkit.command.Command;
+import cn.nukkit.command.CommandSender;
+import cn.nukkit.math.Vector3;
+import cn.nukkit.plugin.PluginBase;
+import cn.nukkit.utils.Config;
+
+import cn.nukkit.Player;
+
+public class WildCommand extends PluginBase {
+
+    Config c;
+
+    public void onEnable() {
+        saveDefaultConfig();
+        c = getConfig();
+    }
+
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (command.getName().equalsIgnoreCase("wild")) {
+            if (sender instanceof Player) {
+                if (c.getList("allowedWorlds").contains(((Player) sender).getLevel().getName())) {
+                    wild((Player) sender);
+                } else {
+                    sender.sendMessage(c.getString("messageUnallowed"));
+                }
+            } else {
+                sender.sendMessage(c.getString("messageInGame"));
+            }
+        }
+
+        return true;
+    }
+
+    private int rand(int min, int max) {
+        if (min == max) {
+            return max;
+        }
+        
+        return min + new Random(System.currentTimeMillis()).nextInt(max - min);
+    }
+
+    private void wild(Player p) {
+        if (p.teleport(p.getLevel().getSafeSpawn(new Vector3(rand(c.getInt("minX"), c.getInt("maxX")), 70, rand(c.getInt("minZ"), c.getInt("maxZ")))))) {
+            p.sendMessage(c.getString("messageSuccess"));
+        } else {
+            p.sendMessage(c.getString("messageError"));
+        }
+    }
+}
