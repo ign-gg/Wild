d9c6b640cdcf11e933149d546d3d83c8d9ac832b
Petteri <26197131+PetteriM1@users.noreply.github.com>
2025-09-19 14:18:10 +0300

    Random spawn

--- src/main/java/tk/suomicraftpe/commands/WildCommand.java
+++ src/main/java/tk/suomicraftpe/commands/WildCommand.java
@@ -1,10 +1,13 @@
 package tk.suomicraftpe.commands;
 
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ThreadLocalRandom;
 
 import cn.nukkit.command.Command;
 import cn.nukkit.command.CommandSender;
-import cn.nukkit.math.Vector3;
+import cn.nukkit.level.Level;
+import cn.nukkit.level.Position;
 import cn.nukkit.plugin.PluginBase;
 import cn.nukkit.utils.Config;
 
@@ -12,7 +15,9 @@ import cn.nukkit.Player;
 
 public class WildCommand extends PluginBase {
 
-    Config c;
+    private Config c;
+
+    private final Set<Long> tpQueue = ConcurrentHashMap.newKeySet();
 
     public void onEnable() {
         saveDefaultConfig();
@@ -22,8 +27,19 @@ public class WildCommand extends PluginBase {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (command.getName().equals("wild")) {
             if (sender instanceof Player) {
-                if (c.getList("allowedWorlds").contains(((Player) sender).getLevel().getName())) {
-                    wild((Player) sender);
+                String world;
+                boolean byName;
+
+                if (args.length > 0) {
+                    world = args[0];
+                    byName = true;
+                } else {
+                    world = ((Player) sender).getLevel().getName();
+                    byName = false;
+                }
+
+                if (c.getList("allowedWorlds").contains(world)) {
+                    wild((Player) sender, world, byName);
                 } else {
                     sender.sendMessage(c.getString("messageUnallowed"));
                 }
@@ -35,7 +51,7 @@ public class WildCommand extends PluginBase {
         return true;
     }
 
-    private int rand(int min, int max) {
+    private static int rand(int min, int max) {
         if (min == max) {
             return max;
         }
@@ -43,13 +59,37 @@ public class WildCommand extends PluginBase {
         return min + ThreadLocalRandom.current().nextInt(max - min);
     }
 
-    private void wild(Player p) {
-        p.level.threadedExecutor.execute(() -> {
-            if (p.teleport(p.getLevel().getSafeSpawn(new Vector3(rand(c.getInt("minX"), c.getInt("maxX")), 128, rand(c.getInt("minZ"), c.getInt("maxZ")))))) {
-                p.sendMessage(c.getString("messageSuccess"));
-            } else {
-                p.sendMessage(c.getString("messageError"));
-            }
+    private void wild(Player p, String world, boolean byName) {
+        if (tpQueue.contains(p.getId())) {
+            return;
+        }
+
+        Level level = getServer().getLevelByName(world);
+        if (level == null) {
+            p.sendMessage(c.getString("messageError"));
+            return;
+        }
+
+        tpQueue.add(p.getId());
+
+        level.threadedExecutor.execute(() -> {
+            Position spawn = level.getSafeSpawn(new Position(rand(
+                    byName ? c.getInt("BminX") : c.getInt("minX"),
+                    byName ? c.getInt("BmaxX") : c.getInt("maxX")),
+                    128, rand(
+                    byName ? c.getInt("BminZ") : c.getInt("minZ"),
+                    byName ? c.getInt("BmaxZ") : c.getInt("maxZ")),
+                    level));
+
+            p.level.threadedExecutor.execute(() -> {
+                tpQueue.remove(p.getId());
+
+                if (p.teleport(spawn)) {
+                    p.sendMessage(c.getString("messageSuccess"));
+                } else {
+                    p.sendMessage(c.getString("messageError"));
+                }
+            });
         });
     }
 }
